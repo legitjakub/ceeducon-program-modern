a333563ba214a26fa9e773cdbf7627ac5159fb8b
jakubhrncir <jakub.hrncir24@gmail.com>
2026-08-20 14:04:36 +0200

    Use CEEDUCON favicon

--- wordpress-theme/ceeducon-program/header.php
+++ wordpress-theme/ceeducon-program/header.php
@@ -6,8 +6,8 @@
     <meta name="theme-color" content="#0d5e9d" />
     <link rel="preload" href="<?php echo esc_url(ceeducon_asset_url('assets/fonts/Tabac-Sans-Regular.woff2')); ?>" as="font" type="font/woff2" crossorigin />
     <link rel="preload" href="<?php echo esc_url(ceeducon_asset_url('assets/fonts/Tabac-Sans-Medium.woff2')); ?>" as="font" type="font/woff2" crossorigin />
-    <link rel="icon" href="<?php echo esc_url(ceeducon_asset_url('assets/favicon.png')); ?>" />
-    <link rel="apple-touch-icon" href="<?php echo esc_url(ceeducon_asset_url('assets/apple-touch-icon.png')); ?>" />
+    <link rel="icon" type="image/png" sizes="64x64" href="<?php echo esc_url(add_query_arg('ver', wp_get_theme()->get('Version'), ceeducon_asset_url('assets/favicon.png'))); ?>" />
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo esc_url(add_query_arg('ver', wp_get_theme()->get('Version'), ceeducon_asset_url('assets/apple-touch-icon.png'))); ?>" />
     <?php wp_head(); ?>
   </head>
   <body <?php body_class(); ?> id="top">
